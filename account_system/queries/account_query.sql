fad14ceba955360047615affd2a18220150724c7
dimy1986 <61546295+dimy1986@users.noreply.github.com>
2026-04-09 08:36:22 +0800

    Add files via upload

--- account_system/queries/account_query.sql
+++ account_system/queries/account_query.sql
@@ -8,15 +8,21 @@
 -- 请确保 {field} 替换后的列名在 Inceptor 表中实际存在。
 -- ============================================================
 
-SELECT
-    统一客户编号,
-    证件号码,
-    借记卡卡号,
-    中文名称,
-    家庭地址,
-    移动电话,
-    开户日期,
-    开户机构名称,
-    开户机构
-FROM accounts
-WHERE {field} LIKE %s
+SELECT DISTINCT 
+a.cst_id 统一客户编号,
+b.crdt_no 证件号码,
+c.dbcrd_cardno 借记卡卡号,
+chn_nm 中文名称,
+fam_adr 家庭地址,
+mblph_no 移动电话,
+opnacc_dt 开户日期,
+opnacc_inst 开户机构,
+opnacc_inst_nm 开户机构名称,
+opnacc_inst_blng_br 开户机构所属分行,
+opnacc_inst_blng_br_nm 开户机构所属分行名称
+FROM bdmf.bdmf_detail_ip_cust_per_info_h a JOIN bdmf.bdmf_detail_ip_cust_per_snstv_info_h b ON a.cst_id=b.cst_id AND 
+ b.start_date<=sysdate-1 AND b.end_date>sysdate-1 AND b.start_date>=substr(sysdate-1,1,8)||'01' 
+JOIN bdmf.bdmf_f_ar_dbcrd_crd_inf_h c ON a.cst_id=c.cst_id AND c.start_date<=sysdate-1 AND c.end_date>sysdate-1 AND c.start_date>=substr(sysdate-1,1,8)||'01'  
+WHERE a.start_date<=sysdate-1 AND a.end_date>sysdate-1 AND a.start_date>=substr(sysdate-1,1,8)||'01'
+AND {field} like %s 
+
